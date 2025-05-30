8871ffa240b243ff47d93eb80277c162bc2e9a9b
Connor <cmaalto03@gmail.com>
2025-05-30 18:44:17 -0400

    css for login (messy)

--- frontend_nx_workspace/apps/auth_app/src/app/app.component.ts
+++ frontend_nx_workspace/apps/auth_app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { HeaderComponent } from './shared/header/header.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'auth_app';
+  title = 'Auth App';
 }
